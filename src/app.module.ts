92740a6e29fc69af3943db2346b624de48d6330d
VergilGX <fabiosotomejia@gmail.com>
2021-12-16 11:30:24 -0500

    feat: Orders Structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { BooksModule } from './books/books.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { OrdersModule } from './orders/orders.module';
 @Module({
-  imports: [PrismaModule, BooksModule, AuthModule, UsersModule],
+  imports: [PrismaModule, BooksModule, AuthModule, UsersModule, OrdersModule],
   controllers: [],
   providers: [],
 })
